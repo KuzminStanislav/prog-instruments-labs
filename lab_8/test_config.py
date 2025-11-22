de0ad1dd521d5006d1a84e54e2306f5a8a40c7c7
KuzminStanislav <a663ve@gmail.com>
2025-11-22 21:12:01 +0400

    parser out of class

--- lab_8/test_config.py
+++ lab_8/test_config.py
@@ -48,52 +48,52 @@ class RequestResult:
     error: Optional[str] = None
 
 
-    def parse_args() -> TestConfig:
+def parse_args() -> TestConfig:
+    """
+    Arguments parsing
+    :return: Test Config
+    """
+    parser = argparse.ArgumentParser(
+        description = "Site load test",
+        formatter_class = argparse.RawDescriptionHelpFormatter,
+        epilog = """
+        Examples:
+        python main.py https://www.bitrix24.ru/prices/ 100 10 5
         """
-        Arguments parsing
-        :return: Test Config
-        """
-        parser = argparse.ArgumentParser(
-            description = "Site load test",
-            formatter_class = argparse.RawDescriptionHelpFormatter,
-            epilog = """
-            Examples:
-            python main.py https://www.bitrix24.ru/prices/ 100 10 5
-            """
-        )
-
-        parser.add_argument(
-            'url',
-            help = 'Site URL' 
-        )
-        parser.add_argument(
-            'rps',
-            type=int,
-            help='RPS'
-        )
-        parser.add_argument(
-            'test_time',
-            type=int, 
-            help='Test duratiom'
-        )
-        parser.add_argument(
-            'wait_timeout',
-            type=int,
-            help='Timeout after test ending'
-        )
-    
-        args = parser.parse_args()
-
-        if args.rps <= 0:
-            raise ValueError("RPS must be > 0")
-        if args.test_time <= 0:
-            raise ValueError("Test time must be > 0")
-        if args.wait_timeout <= 0:
-            raise ValueError("Timeout must be > 0")
-        
-        return TestConfig(
-        url=args.url,
-        rps=args.rps,
-        test_time=args.test_time,
-        wait_timeout=args.wait_timeout
     )
+
+    parser.add_argument(
+        'url',
+        help = 'Site URL' 
+    )
+    parser.add_argument(
+        'rps',
+        type=int,
+        help='RPS'
+    )
+    parser.add_argument(
+        'test_time',
+        type=int, 
+        help='Test duratiom'
+    )
+    parser.add_argument(
+        'wait_timeout',
+        type=int,
+        help='Timeout after test ending'
+    )
+
+    args = parser.parse_args()
+
+    if args.rps <= 0:
+        raise ValueError("RPS must be > 0")
+    if args.test_time <= 0:
+        raise ValueError("Test time must be > 0")
+    if args.wait_timeout <= 0:
+        raise ValueError("Timeout must be > 0")
+    
+    return TestConfig(
+    url=args.url,
+    rps=args.rps,
+    test_time=args.test_time,
+    wait_timeout=args.wait_timeout
+)
